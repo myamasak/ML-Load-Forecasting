a8b2ba91742befa612dd295ef2fd6ba53bf1228b
Marcos Yamasaki <yamasaki.marcos@gmail.com>
2022-01-20 07:33:07 -0300

    Script to rename easily bunch of files

---
+++ src/rename_files.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import glob
+path = os.path.realpath(__file__)
+path = r'%s' % path.replace(
+    f'\\{os.path.basename(__file__)}', '').replace('\\', '/')
+if path.find('autoML') != -1:
+    path = r'%s' % path.replace('/autoML', '')
+elif path.find('src') != -1:
+    path = r'%s' % path.replace('/src', '')
+
+
+DATASET_NAME = 'ISONewEngland'
+
+#path = path + '/dataset/{DATASET_NAME}/custom/*.csv'
+
+files = glob.glob(path + f'/datasets/{DATASET_NAME}/custom/*.csv')
+
+# files = os.listdir(path)
+
+for index, file in enumerate(files):
+    if file.find('forecast7') != -1:
+        print(file)
+        print('Renaming file...')
+    
+        filenew = file.replace('_forecast7', '')
+        print(filenew)
+        os.rename(file, filenew)
+#        break
